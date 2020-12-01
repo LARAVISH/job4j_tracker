3627519fd3d945660eda131ae0c45f11d3cd3d64
LARAVISH <fandvor@mail.ru>
2020-12-01 15:54:20 +0500

    Change ids type.

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -22,6 +22,6 @@ public class Point {
     }
 
     public double distance3d(Point that) {
-        return Math.sqrt((distance(that) + Math.pow(this.z - that.z, 2)));
+        return Math.sqrt((Math.pow(this.x - that.x,2)+ Math.pow(this.y - that.y,2) + Math.pow(this.z - that.z, 2)));
     }
 }
