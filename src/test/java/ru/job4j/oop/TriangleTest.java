a1f3e92d378b2717d14a91b9cca6bf6f5581c785
LARAVISH <fandvor@mail.ru>
2020-12-01 14:01:00 +0500

    Change ids type.

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TriangleTest extends TestCase {
+
+    public void testPeriod() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
+    }
+}
