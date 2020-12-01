6ee2a43178f44c32729719bbed39a518bd92ce90
LARAVISH <fandvor@mail.ru>
2020-12-01 14:20:02 +0500

    Change ids type.

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Max {
+    public double max(double first, double second) {
+        return Math.max(first, second);
+    }
+
+    public double max(double first, double second, double third) {
+        return Math.max(max(first, second), third);
+    }
+
+    public double max(double first, double second, double third, double fourth) {
+        return Math.max(max(first, second, third), fourth);
+    }
+
+    public static void main(String[] args) {
+        Max maximum = new Max();
+        System.out.println(maximum.max(200, 500, 8000, 100));
+    }
+}
