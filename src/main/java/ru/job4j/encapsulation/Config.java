29c3826b966dfaf6c955f2ae632f41ba10352749
LARAVISH <fandvor@mail.ru>
2020-12-04 11:17:38 +0500

    Change ids type.

---
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -0,0 +1,19 @@
+package ru.job4j.encapsulation;
+
+public class Config {
+    private String name;
+    private int position;
+    private String[] properties;
+
+    public Config(String name) {
+        System.out.println(position);
+    }
+
+    public String getProperty(String key) {
+        return search(key);
+    }
+
+    public String search(String key) {
+        return key;
+    }
+}
