5ca8e00020c993e6dd3649bb67749db167aa5068
LARAVISH <fandvor@mail.ru>
2020-12-07 18:03:14 +0500

    Change ids type.

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,25 +57,27 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean res = index != -1;
+        if (res) {
             item.setId(id);
             items[index] = item;
-            return true;
+            return res;
         }
-        return false;
+        return res;
     }
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean res = index != -1;
+        if (res) {
             int start = index + 1;
             int distPos = index;
             int length = size - index;
             System.arraycopy(items, start, items, distPos, length);
             items[size - 1] = null;
             size--;
-            return true;
+            return res;
         }
-        return false;
+        return res;
     }
 }
