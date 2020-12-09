ab4d5f5746d85c27ede96dd9baf8d062c4626621
LARAVISH <fandvor@mail.ru>
2020-12-09 16:30:59 +0500

    Change ids type.

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -8,9 +8,8 @@ public class MagicBall {
     public static void main(String[] args) {
         Scanner input = new Scanner((System.in));
         System.out.print("\"Я великий Оракул. Что ты хочешь узнать? \"");
+        String name = input.nextLine();
         int answer = new Random().nextInt(3);
-        answer = input.nextInt();
-
         if (answer == 0) {
             System.out.println("Да");
         } else if (answer == 1) {
