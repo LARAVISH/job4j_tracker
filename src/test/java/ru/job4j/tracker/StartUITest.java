cf5f3b6e7a148d9dd3699bc2d8ade0057105268e
LARAVISH <fandvor@mail.ru>
2020-12-16 11:55:17 +0500

    Change ids type.

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+
+public class StartUITest extends TestCase {
+
+    @Test
+    public void testCreateItem() {
+        String[] answers = {"Update"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Update");
+        assertThat(created.getName(), is(expected.getName()));
+    }
+}
