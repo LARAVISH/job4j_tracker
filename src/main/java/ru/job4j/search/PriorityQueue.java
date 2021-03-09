d56c4a8f30a35ab4da0d9c928b9da42ddc56f745
LARAVISH <fandvor@mail.ru>
2021-03-09 19:29:03 +0500

    Change ids type.

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,19 +5,18 @@ import java.util.LinkedList;
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
-    public void put(Task task){
+    public void put(Task task) {
         int index = 0;
-        for(Task element : tasks){
-            if(element.getPriority() < task.getPriority()){
-                index++;
-            }else{
+        for (Task element : tasks) {
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
-
+            index++;
         }
-        this.tasks.add(index,task);
+        this.tasks.add(index, task);
     }
-    public Task take(){
+
+    public Task take() {
         return tasks.poll();
     }
 }
