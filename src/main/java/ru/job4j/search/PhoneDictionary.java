9ccd7cede5b4680461d71c8a6a612f82bdb64d84
LARAVISH <fandvor@mail.ru>
2021-03-10 10:53:26 +0500

    Change ids type.

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -3,15 +3,15 @@ package ru.job4j.search;
 import java.util.ArrayList;
 
 public class PhoneDictionary {
-    private ArrayList<Person> person = new ArrayList<>();
+    private ArrayList<Person> list = new ArrayList<>();
 
     public void add(Person person) {
-        this.person.add(person);
+        this.list.add(person);
     }
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        for (Person value : person) {
+        for (Person value : list) {
             if (value.getSurname().contains(key) || value.getName().contains(key) ||
                     value.getPhone().contains(key) || value.getAddress().contains(key)) {
                 result.add(value);
