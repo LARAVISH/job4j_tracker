143425a35493fb1b81ea5b645dde2bfd66939cf2
LARAVISH <fandvor@mail.ru>
2021-03-26 21:06:15 +0500

    Change ids type.

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,45 @@
+package ru.job4j.collection;
+
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User user) {
+
+        int res = this.name.compareTo(user.getName());
+        if (res == 0) {
+            res = Integer.compare(this.age, user.getAge());
+        }
+        return res;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
