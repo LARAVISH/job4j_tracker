b1c435b9093835d89c6405c5b6c2def80109c762
LARAVISH <fandvor@mail.ru>
2021-05-29 10:56:47 +0500

    Change ids type.

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,15 +1,12 @@
 package ru.job4j.collection;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
         Map<Character, Integer> map = new HashMap<>();
-        if (left.length() != right.length()) {
-            return false;
-        }
+
         for (Character el : left.toCharArray()) {
             if (!map.containsKey(el)) {
                 map.put(el, 1);
@@ -20,15 +17,15 @@ public class FreezeStr {
         for (Character el : right.toCharArray()) {
             if (!map.containsKey(el)) {
                 return false;
-            } else {
+            } else if (map.containsKey(el) && map.get(el) == 1) {
+                map.remove(el);
+            } else if (map.containsKey(el) && map.get(el) > 1) {
                 map.put(el, map.get(el) - 1);
             }
-        }
-        for (Character el : map.keySet()) {
-            if (map.get(el) != 0) {
-                return false;
+            if (map.isEmpty()) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
