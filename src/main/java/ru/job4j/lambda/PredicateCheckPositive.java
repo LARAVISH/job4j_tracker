278d4efde8afa7964d159325fb283f15b89380a2
LARAVISH <fandvor@mail.ru>
2021-06-07 17:59:48 +0500

    Change ids type.

--- src/main/java/ru/job4j/lambda/PredicateCheckPositive.java
+++ src/main/java/ru/job4j/lambda/PredicateCheckPositive.java
@@ -5,10 +5,12 @@ import java.util.function.Predicate;
 public class PredicateCheckPositive {
 
     public static boolean check(int num) {
-        return num > 0;
+
+        return check( b -> b > 0, num);
     }
 
     private static boolean check(Predicate<Integer> predicate, int num) {
-        return predicate.test(num);
+
+        return predicate.test(num );
     }
 }
