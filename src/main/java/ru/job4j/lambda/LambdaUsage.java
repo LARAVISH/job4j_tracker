2864f9737686d9524b6d7f94323d963d36f1e36d
LARAVISH <fandvor@mail.ru>
2021-06-11 13:18:20 +0500

    Change ids type.

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,12 +1,17 @@
 package ru.job4j.lambda;
 
-import java.util.Comparator;
+import java.util.*;
 
 public class LambdaUsage {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("hello");
+
         Comparator<String> comparator = (left, right) -> {
             System.out.println("compare - " + right.length() + " : " + left.length());
             return Integer.compare(right.length(), left.length());
         };
+        list.sort(comparator);
     }
 }
