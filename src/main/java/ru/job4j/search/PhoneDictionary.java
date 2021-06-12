de1c421cb136027677097232caac5aaf8e39a272
LARAVISH <fandvor@mail.ru>
2021-06-12 16:45:53 +0500

    Change ids type.

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,22 +1,28 @@
 package ru.job4j.search;
 
+
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private final ArrayList<Person> list = new ArrayList<>();
+    private ArrayList<Person> list = new ArrayList<>();
 
     public void add(Person person) {
         this.list.add(person);
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person value : list) {
-            if (value.getSurname().contains(key) || value.getName().contains(key) ||
-                    value.getPhone().contains(key) || value.getAddress().contains(key)) {
-                result.add(value);
+        ArrayList<Person> personList = new ArrayList<>();
+        Predicate<Person> namePredicate = s -> s.getName().contains(key);
+        Predicate<Person> surnamePredicate = s -> s.getSurname().contains(key);
+        Predicate<Person> phonePredicate = s -> s.getPhone().contains(key);
+        Predicate<Person> addressPredicate = s -> s.getAddress().contains(key);
+        Predicate<Person> combinePredicate = namePredicate.or(surnamePredicate).or(phonePredicate).or(addressPredicate);
+        for (Person person : list) {
+            if (combinePredicate.test(person)) {
+                personList.add(person);
             }
         }
-        return result;
+        return personList;
     }
 }
