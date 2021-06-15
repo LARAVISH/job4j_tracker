dc267d016e10915b880203ecacefde9d501e1574
LARAVISH <fandvor@mail.ru>
2021-06-15 12:15:07 +0500

    Change ids type.

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+
+
+class ProfilesTest {
+
+    @Test
+    void collect() {
+        List<Address> expect = List.of(new Address("Moscow","Lenina",20,199),
+        new Address("Moscow","Mira",68,857),
+                new Address("Moscow","Vtoray",3,888));
+       Profiles profiles = new Profiles();
+
+       List<Profile> profileList = List.of(new Profile(new Address("Moscow","Lenina",20,199)),
+               new Profile(new Address("Moscow","Mira",68,857)),
+               new Profile(new Address("Moscow","Vtoray",3,888)));
+       List<Address> res = profiles.collect(profileList);
+        MatcherAssert.assertThat(res, is(expect));
+    }
+}
