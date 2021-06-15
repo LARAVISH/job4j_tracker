1dfe5f44839b0563b386f61a174596870f7dadbd
LARAVISH <fandvor@mail.ru>
2021-06-15 13:51:27 +0500

    Change ids type.

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    public List<Address> collect(List<Profile> profiles){
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+    public List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -12,15 +12,16 @@ class ProfilesTest {
 
     @Test
     void collect() {
-        List<Address> expect = List.of(new Address("Moscow","Lenina",20,199),
-        new Address("Moscow","Mira",68,857),
-                new Address("Moscow","Vtoray",3,888));
-       Profiles profiles = new Profiles();
+        List<Address> expect = List.of(new Address("Moscow", "Lenina", 20, 199),
+                new Address("Moscow", "Mira", 68, 857),
+                new Address("Moscow", "Vtoray", 3, 888));
+        Profiles profiles = new Profiles();
 
-       List<Profile> profileList = List.of(new Profile(new Address("Moscow","Lenina",20,199)),
-               new Profile(new Address("Moscow","Mira",68,857)),
-               new Profile(new Address("Moscow","Vtoray",3,888)));
-       List<Address> res = profiles.collect(profileList);
+        List<Profile> profileList = List.of(new Profile
+                        (new Address("Moscow", "Lenina", 20, 199)),
+                new Profile(new Address("Moscow", "Mira", 68, 857)),
+                new Profile(new Address("Moscow", "Vtoray", 3, 888)));
+        List<Address> res = profiles.collect(profileList);
         MatcherAssert.assertThat(res, is(expect));
     }
 }
