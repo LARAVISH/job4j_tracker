800e5d08c7f44efbd989ca581985fed3102013b2
LARAVISH <fandvor@mail.ru>
2021-06-17 18:35:25 +0500

    Change ids type.

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> list = new ArrayList<>();
+    private final ArrayList<Person> list = new ArrayList<>();
 
     public void add(Person person) {
         this.list.add(person);
@@ -17,8 +17,8 @@ public class PhoneDictionary {
         Predicate<Person> surnamePredicate = s -> s.getSurname().contains(key);
         Predicate<Person> phonePredicate = s -> s.getPhone().contains(key);
         Predicate<Person> addressPredicate = s -> s.getAddress().contains(key);
-        Predicate<Person> combinePredicate = namePredicate.or(surnamePredicate).or(phonePredicate).or(addressPredicate);
-        for (Person person : list) {
+        var combinePredicate = namePredicate.or(surnamePredicate).or(phonePredicate).or(addressPredicate);
+        for (var person : list) {
             if (combinePredicate.test(person)) {
                 personList.add(person);
             }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -11,37 +11,37 @@ import static org.junit.Assert.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void testFindName() {
-        PhoneDictionary phoneDictionary = new PhoneDictionary();
+        var phoneDictionary = new PhoneDictionary();
         phoneDictionary.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> person = phoneDictionary.find("Petr");
+        var person = phoneDictionary.find("Petr");
         assertThat(person.get(0).getSurname(), is("Arsentev"));
     }
     @Test
     public void testFindSurname() {
-        PhoneDictionary phoneDictionary = new PhoneDictionary();
+        var phoneDictionary = new PhoneDictionary();
         phoneDictionary.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> person = phoneDictionary.find("Arsentev");
+        var person = phoneDictionary.find("Arsentev");
         assertThat(person.get(0).getPhone(), is("534872"));
     }
     @Test
     public void testFindPhone() {
-        PhoneDictionary phoneDictionary = new PhoneDictionary();
+        var phoneDictionary = new PhoneDictionary();
         phoneDictionary.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> person = phoneDictionary.find("534872");
+        var person = phoneDictionary.find("534872");
         assertThat(person.get(0).getAddress(), is("Bryansk"));
     }
     @Test
     public void testFindAddress() {
-        PhoneDictionary phoneDictionary = new PhoneDictionary();
+        var phoneDictionary = new PhoneDictionary();
         phoneDictionary.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> person = phoneDictionary.find("Bryansk");
+        var person = phoneDictionary.find("Bryansk");
         assertThat(person.get(0).getName(), is("Petr"));
     }
     @Test
     public void testNotFind() {
-        PhoneDictionary phoneDictionary = new PhoneDictionary();
+        var phoneDictionary = new PhoneDictionary();
         phoneDictionary.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> person = phoneDictionary.find("hhh");
+        var person = phoneDictionary.find("hhh");
         assertThat(new ArrayList<Person>(), is(person));
 
     }
