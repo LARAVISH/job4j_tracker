9255c76ebfc60dfd0bf3b411da8f4cecfd51806a
LARAVISH <fandvor@mail.ru>
2021-06-17 15:05:04 +0500

    Change ids type.

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -1,16 +1,31 @@
 package ru.job4j.stream;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 public class Card {
+    private final Suit suit;
+    private final Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit=" + suit +
+                ", value=" + value +
+                '}';
+    }
 
     public static void main(String[] args) {
-        Suit[] suits = {Suit.Diamonds, Suit.Hearts, Suit.Spades, Suit.Clubs};
-        Value[] values = {Value.V_6, Value.V_7, Value.V_8};
-        Stream.of(suits)
-                .flatMap(suit -> Stream.of(values)
-                        .map(value -> suit + " " + value))
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> new Card(suit, value)))
+                .collect(Collectors.toList())
                 .forEach(System.out::println);
     }
 }
